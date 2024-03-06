bc64552947284ec3acfa01df57f1b9082c5add1b
ChristinaDian <krisdian2001@gmail.com>
2024-03-06 11:56:19 +0200

    Init Downsizing method with bitmap data

--- ImageDownsizer/Form1.Designer.cs
+++ ImageDownsizer/Form1.Designer.cs
@@ -28,12 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnOpenFileDialog = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpenFileDialog
+            // 
+            this.btnOpenFileDialog.Location = new System.Drawing.Point(38, 62);
+            this.btnOpenFileDialog.Name = "btnOpenFileDialog";
+            this.btnOpenFileDialog.Size = new System.Drawing.Size(218, 35);
+            this.btnOpenFileDialog.TabIndex = 0;
+            this.btnOpenFileDialog.Text = "Select file";
+            this.btnOpenFileDialog.UseVisualStyleBackColor = true;
+            this.btnOpenFileDialog.Click += new System.EventHandler(this.btnOpenFileDialog_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 175);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(377, 263);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(406, 175);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(382, 263);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(314, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select down-scaling factor in %";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(335, 75);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Tag = "";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(531, 61);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(218, 37);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Proceed";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(132, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Original Image";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(531, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Downsized Image";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnOpenFileDialog);
+            this.Name = "Form1";
+            this.Text = "Image Downsizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btnOpenFileDialog;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private NumericUpDown numericUpDown1;
+        private Button btnSubmit;
+        private Label label2;
+        private Label label3;
     }
 }
--- ImageDownsizer/Form1.cs
+++ ImageDownsizer/Form1.cs
@@ -1,10 +1,78 @@
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
 namespace ImageDownsizer
 {
     public partial class Form1 : Form
     {
+        private Bitmap originalImage;
+        private Bitmap downscaledImage;
+        private BitmapData originalImageData;
+        private BitmapData downscaledImageData;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpenFileDialog_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Bitmap(fileDialog.FileName);
+                downscaledImage  = new Bitmap(fileDialog.FileName);
+                pictureBox1.Image = originalImage;
+            }
+        } 
+        
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null)
+            {
+                MessageBox.Show("Please select a file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int downscaleFactor = (int)numericUpDown1.Value;   
+            DownscaleMethod(downscaleFactor);
+            pictureBox2.Image = downscaledImage;
+        }
+        private void DownscaleMethod(int downscaleFactor)
+        {
+            // Lock the bitmap's bits.  
+            Rectangle rect = new Rectangle(0, 0, downscaledImage.Width, downscaledImage.Height);
+            BitmapData bmpData =
+                downscaledImage.LockBits(rect, ImageLockMode.ReadWrite,
+                downscaledImage.PixelFormat);
+
+            // Get the address of the first line.
+            IntPtr ptr = bmpData.Scan0;
+
+            // Declare an array to hold the bytes of the bitmap.
+            int bytes = Math.Abs(bmpData.Stride) * downscaledImage.Height;
+            byte[] rgbValues = new byte[bytes];
+
+            // Copy the RGB values into the array.
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            // Set every third value to 255. A 24bpp bitmap will look red.  
+            for (int counter = 2; counter < rgbValues.Length; counter += 3)
+                rgbValues[counter] = 255;
+
+            // Copy the RGB values back to the bitmap
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+            // Unlock the bits.
+            downscaledImage.UnlockBits(bmpData);
+
+            // Draw the modified image.
+            //e.Graphics.DrawImage(originalImage, 0, 150);
+        }
+
+    
     }
 }
